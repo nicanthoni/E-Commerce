8c0bcd88462ff89863762493d4e649c9478d6c34
nic-d <nicanthonidiaz@gmail.com>
2024-02-24 16:50:35 -0500

    working on app working

---
+++ client/src/layouts/RootLayout.jsx
@@ -0,0 +1,14 @@
+// RootLayout.jsx
+import { Typography, Grid } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+export default function RootLayout() {
+//   return (
+//     <Grid container>
+//       <Grid item>
+//         <Typography color={{ primary: red[500] }}>Root Layout Working</Typography>
+//       </Grid>
+//     </Grid>
+//   );
+}
+
---
+++ client/src/pages/test.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import { Typography } from "@mui/material";
+
+export default function Test() {
+  return (
+  <React.Fragment>
+  <Typography variant="h1">Test passed ✅</Typography>
+  </React.Fragment>
+  )
+}
