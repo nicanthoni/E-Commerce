50e354be74129746e7475dbf4c21c5e74ea00eda
nic-d <nicanthonidiaz@gmail.com>
2024-03-12 12:54:01 -0400

    More file structure added for client side / front end

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar () {
+    return (
+        <>
+        {/* NavBar component to be added to RootLayout */}
+        </>
+    )
+}
--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,14 +1,17 @@
-// RootLayout.jsx
+// RootLayout to hold NavBar, Outlet, and Footer components
+
 import { Typography, Grid } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-<>
+    <>
+      <Typography>Navbar Component</Typography>
       <main>
         <Outlet />
       </main>
-</>
+      <Typography>Footer</Typography>
+    </>
   );
 }
