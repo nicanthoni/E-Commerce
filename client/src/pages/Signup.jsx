3a85aacc4636afb5723cabf35e0ec4cc3063d293
nic-d <nicanthonidiaz@gmail.com>
2024-03-18 16:00:28 -0400

    MUI now working on signup page; wasn't allowing me to use mui components initially

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,13 +1,14 @@
 // This page will conditionally render either the Vendor or Buyer signup form
 import React from "react"
+import { Typography } from "@mui/material"
  
 
 export default function Signup () {
     return (
         <>
-        <div>
-            Sign up component
-        </div>
+        <Typography variant="h3">
+            Sign up/in component
+        </Typography>
         </>
     )
 }
