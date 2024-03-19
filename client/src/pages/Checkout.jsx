088889d17f6226eb08aab506805d862878fc3ec8
nic-d <nicanthonidiaz@gmail.com>
2024-03-19 18:45:52 -0400

    new checkout page renders when checkout button from drawer clicked

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,11 +1,10 @@
-import CheckoutMain from "../components/Checkout/CheckoutMain";
-import { Box } from "@mui/material";
+import {  Typography } from "@mui/material";
 
 
 export default function Checkout () {
     return (
-        <Box className='checkout-container'>
-        <CheckoutMain/>
-        </Box>
+        <Typography>
+            Checkout Page
+        </Typography>
     )
 }
