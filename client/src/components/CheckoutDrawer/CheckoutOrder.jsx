95f8eddb3a68ff0396bf405403c180170219566e
nic-d <nicanthonidiaz@gmail.com>
2024-03-21 17:01:04 -0400

    replaced card componenet usage with the Stack and box components.

--- client/src/components/CheckoutDrawer/CheckoutOrder.jsx
+++ client/src/components/CheckoutDrawer/CheckoutOrder.jsx
@@ -1,41 +1,39 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import productPhoto from "../../assets/images/shirt.jpg";
-import { Divider } from "@mui/material";
+import {
+  Stack,
+  Box,
+  Typography,
+  Divider,
+} from "@mui/material";
 
 // ORDER SUMMARY
 export default function CheckoutOrder() {
   return (
-    <Card
-      className="order-container"
-      sx={{ display: "flex", maxWidth: 245, padding: 1 }}
-      elevation={1}
-    >
-      <CardMedia
-        className="order-img"
-        component="img"
-        sx={{ width: "50%", height: "50%", objectFit: "contain" }}
-        src={productPhoto}
-        title="Product Name"
-      />
-      <CardContent className="order-description-container">
-        <Typography variant="h6">
-          Product Name
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Category:
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Quantity:
-        </Typography>
-        <CardActions sx={{}}>
-          <Button size="small">Remove</Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+    <Stack 
+    borderBottom={'inset'} 
+    direction={"row"} 
+    gap={1} 
+    padding={1}
+    margin={1}
+    justifyContent={'center'} 
+    alignItems={'center'} 
+ >
+        <Box sx={{height: "100px", width: "100px"}}>
+          <img src={productPhoto}
+            alt="Product"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain" // ensures  image fits within the Box
+            }}
+          />
+        </Box>
+      <Stack spacing={1} divider={<Divider orientation="horizontal"/>}>
+        <Typography variant="caption"> Product Name </Typography>
+        <Typography variant="caption"> Product Category </Typography>
+        <Typography variant="caption"> Quantity: </Typography>
+        <Typography variant="caption"> Item Price: </Typography>
+      </Stack>
+    </Stack>
   );
 }
