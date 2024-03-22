bb76f9df471e30a791348bd0836e9211daf69f05
nic-d <nicanthonidiaz@gmail.com>
2024-03-22 16:40:53 -0400

    front end drawer updates

--- client/src/components/CheckoutDrawer/CheckoutMain.jsx
+++ client/src/components/CheckoutDrawer/CheckoutMain.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import Divider from "@mui/material/Divider";
+
 import CheckoutOrder from "./CheckoutOrder";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Button, Typography } from "@mui/material";
@@ -21,31 +21,27 @@ export default function CheckoutMain() {
   const DrawerList = (
     <Grid
       container
-      direction={'row'}
+      direction='row'
       sx={{ width: 250, height: '100%', textAlign: "center" }}
       onClick={toggleDrawer(false)}
       rowSpacing={2}
       margin={0}
-    
+      alignItems="flex-start"
     >
-      <Grid item sx={{ bgcolor: "primary.main" }} xs={12} alignSelf={'flex-start'}>
+      <Grid item sx={{ bgcolor: "primary.main" }} xs={12} alignSelf={'flex-start'} >
         <ShoppingCartIcon sx={{ my: 2  }}/>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} alignSelf='flex-start'>
         <CheckoutOrder />
       </Grid>
-       <Grid item xs={12} alignSelf={'flex-end'}sx={{ padding: 0  }}>
-        <Typography fontWeight={'bold'}>
-           Total: $
-        </Typography>
+       <Grid item xs={12} alignSelf='flex-end' sx={{ padding: 0  }}>
         <Box sx={{ bgcolor: "primary.main" }} xs={12}>
+        <Typography paddingTop={2} fontWeight={'bold'}>Total: $</Typography>
         <Button
           variant="contained"
           href="/checkout"
-          sx={{ bgcolor: "fourth.main", color: "white", my: 2  }}
-        >
-          Checkout
-       </Button>
+          sx={{ bgcolor: "fourth.main", color: "white", marginBottom: 2  }}
+        >Checkout</Button>
        </Box>
       </Grid>
     </Grid>
--- client/src/components/CheckoutDrawer/CheckoutOrder.jsx
+++ client/src/components/CheckoutDrawer/CheckoutOrder.jsx
@@ -10,12 +10,12 @@ import {
 export default function CheckoutOrder() {
   return (
     <Stack 
-    borderBottom={'inset'} 
-    direction={"row"} 
+    borderBottom='inset' 
+    direction='row' 
     gap={1} 
     padding={1}
-    justifyContent={'center'} 
-    alignItems={'center'} 
+    justifyContent='center' 
+    alignItems='center'
     margin={1}
  >
         <Box sx={{height: "100px", width: "100px"}}>
@@ -24,15 +24,15 @@ export default function CheckoutOrder() {
             style={{
               width: "100%",
               height: "100%",
-              objectFit: "contain" // ensures  image fits within the Box
+              objectFit: "contain"// ensures  image fits within the Box
             }}
           />
         </Box>
       <Stack spacing={1} divider={<Divider orientation="horizontal"/>}>
         <Typography variant="caption" fontWeight={'bold'}sx={{fontSize: 10}}>Product Name</Typography>
-        <Typography variant="caption"sx={{fontSize: 10}}> Quantity: </Typography>
-        <Typography variant="caption"sx={{fontSize: 10}}> Item Price: </Typography>
-        <Typography variant="caption"sx={{fontSize: 10}}> Product Category </Typography>
+        <Typography variant="caption"sx={{fontSize: 10}}>Item Price:</Typography>
+        <Typography variant="caption"sx={{fontSize: 10}}>Product Category</Typography>
+        <Typography variant="caption"sx={{fontSize: 10}}>Quantity:</Typography>
       </Stack>
     </Stack>
   );
