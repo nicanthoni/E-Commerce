7f7a2e3d42224ea4147fe880cdedab5b4a994228
nic-d <nicanthonidiaz@gmail.com>
2024-03-25 12:46:21 -0400

    created Vendor Add Item page, where vendors where upload a new product for sell by providing vendor/product details and uploading a image

---
+++ client/src/components/Vendor/AddItemForm.jsx
@@ -0,0 +1,111 @@
+import { Typography } from "@mui/material";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import UploadButton from "./UploadButton";
+
+  
+  // TODO remove, this demo shouldn't need to reset the theme.
+  
+  const defaultTheme = createTheme();
+
+export default function AddItemForm() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          vendor: data.get("vendor"),
+          email: data.get("email"),
+          password: data.get("password"),
+        });
+      };
+    
+      return (
+        <ThemeProvider theme={defaultTheme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar> */}
+              <Typography component="h1" variant="h5">
+                Add Product
+              </Typography>
+              <Box margin={1}>
+              <UploadButton/>
+              </Box>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="vendorName"
+                      label="Vendor Name"
+                      name="vendor"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="vendorProductName"
+                      label="Product Name"
+                      name="product"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="description"
+                      label="Product Description"
+                      id="vendorProductDescription"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="price"
+                      label="Price"
+                      id="vendorProductPrice"
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Submit 
+                </Button>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      )
+}
