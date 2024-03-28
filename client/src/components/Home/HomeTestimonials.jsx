70851889c04f323c34b13c161c34137682bb4290
nic-d <nicanthonidiaz@gmail.com>
2024-03-28 18:27:17 -0400

    added containers where they should be

--- client/src/components/Home/HomeTestimonials.jsx
+++ client/src/components/Home/HomeTestimonials.jsx
@@ -1,8 +1,29 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import { Box, Paper } from "@mui/material";
+
 // import ratings
-// import paper(?)
+
 
 export default function HomeTestimonials() {
-    return (
-        <div>test</div>
-    )
+  return (
+    <Box display="flex" flexWrap="nowrap">
+      <Paper elevation={1}>
+        <Stack>
+
+        </Stack>
+      </Paper>
+      <Paper elevation={1}>
+        <Stack>
+            
+        </Stack>
+      </Paper>
+      <Paper elevation={1}>
+        <Stack>
+
+        </Stack>
+      </Paper>
+    </Box>
+  );
 }
