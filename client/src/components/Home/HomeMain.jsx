642dfbd8460aababf2137aa457bd17ed332d247b
nic-d <nicanthonidiaz@gmail.com>
2024-03-29 21:00:53 -0400

    home page updates

--- client/src/components/Home/HomeMain.jsx
+++ client/src/components/Home/HomeMain.jsx
@@ -1,53 +1,53 @@
+import Grid from "@mui/material/Unstable_Grid2";
+import { Box, Container } from "@mui/material";
+import "./HomeMain.css";
 import HomeInfo from "./HomeInfo";
 import HomeLogo from "./HomeLogo";
 import HomeServices from "./HomeServices";
 import HomeTestimonials from "./HomeTestimonials";
-import "./HomeMain.css";
-import Grid from "@mui/material/Unstable_Grid2";
-import { Box, Container } from "@mui/material";
+import HomeCTA from "./HomeCTA";
 import Footer from "../Footer/Footer";
 
 // Parent Home component
 export default function HomeMain() {
   return (
     <>
-    <Box style={{ backgroundColor: "#024959" }}>
-      <Container maxWidth="xl" className="home-main-container">
-        <Grid container spacing={3} alignItems={"center"}>
-          {/* Text Section */}
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={6}
-            className="homeInfo"
-            sx={{ marginTop: { xs: 9, md: 0 } }}
-          >
-            <HomeInfo />
-          </Grid>
+      <Box style={{ backgroundColor: "#024959" }}>
+        <Container maxWidth="xl" className="home-main-container">
+          <Grid container spacing={3} alignItems={"center"}>
+            {/* Text Section */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={6}
+              className="homeInfo"
+              sx={{ marginTop: { xs: 9, md: 0 } }}
+            >
+              <HomeInfo />
+            </Grid>
 
-          {/* Logo Section */}
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={6}
-            className="homeLogo"
-            sx={{ padding: { xs: 6, sm: 4, md: 2 } }}
-          >
-            <HomeLogo />
+            {/* Logo Section */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={6}
+              className="homeLogo"
+              sx={{ padding: { xs: 6, sm: 4, md: 2 } }}
+            >
+              <HomeLogo />
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
       </Box>
 
       {/* Services Section */}
-        <Box style={{ backgroundColor: "#FFF", width: "100%" }} padding={2}>
-          <Container maxWidth="xl">
-            <HomeServices />
-          </Container>
-        </Box>
-
+      <Box style={{ backgroundColor: "#FFF", width: "100%" }} padding={2}>
+        <Container maxWidth="xl">
+          <HomeServices />
+        </Container>
+      </Box>
 
       {/* Testimonial Section */}
       <Box style={{ backgroundColor: "#F2F2F2", width: "100%" }}>
@@ -57,12 +57,14 @@ export default function HomeMain() {
       </Box>
 
       {/* Call to Action section with Button */}
-
+      <Box style={{ backgroundColor: "#024959", width: "100%" }}>
+        <Container maxWidth="xl">
+          <HomeCTA />
+        </Container>
+      </Box>
 
       {/* Footer - add to root-layout?*/}
-      <Footer/>
-
-
-      </>
+      <Footer />
+    </>
   );
 }
