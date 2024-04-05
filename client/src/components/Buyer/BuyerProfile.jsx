b1d68e4938a298b6e2a3d0554e8c056f5c8d6c97
nic-d <nicanthonidiaz@gmail.com>
2024-04-04 20:43:41 -0400

    user model, AddUser mutation from TypeDef, and AddUser mutation from resolver ALL updated naming convention of lastName and firstName fields

---
+++ client/src/components/Buyer/BuyerProfile.jsx
@@ -0,0 +1,13 @@
+import { Typography, Box, Container } from "@mui/material";
+
+export default function BuyerProfile () {
+ return (
+    <Container maxWidth='xl'>
+        <Box>
+            <Typography>
+                Welcome, Buyer!
+            </Typography>
+        </Box>
+    </Container>
+ )
+}
---
+++ client/src/pages/Buyer/BuyerAccount.jsx
@@ -0,0 +1,10 @@
+import BuyerProfile from "../../components/Buyer/BuyerProfile"
+import { Container } from "@mui/material"
+
+export default function BuyerAccount () {
+    return (
+        <Container>
+                <BuyerProfile/>
+        </Container>
+    )
+}
