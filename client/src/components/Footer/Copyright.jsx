6a499822bb2b0c847614cab66670e1268b7640a4
nic-d <nicanthonidiaz@gmail.com>
2024-04-15 15:16:42 -0400

    created component for copyright copy

---
+++ client/src/components/Footer/Copyright.jsx
@@ -0,0 +1,19 @@
+import { Typography, Link } from "@mui/material";
+
+export default function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="/">
+          AppName
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
--- client/src/components/Signin/VendorSignin.jsx
+++ client/src/components/Signin/VendorSignin.jsx
@@ -1,4 +1,4 @@
-
+import Copyright from '../Footer/Copyright';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -11,23 +11,6 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
 
 export default function Signin() {
   const handleSubmit = (event) => {
