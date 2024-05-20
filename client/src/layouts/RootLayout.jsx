0712fc869c52af9111508a4cc654bb8c257d2b36
nic-d <nicanthonidiaz@gmail.com>
2024-05-19 20:13:46 -0400

    added bottom nav to profile when user logs in

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,13 +1,19 @@
 import Navbar from '../pages/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import BottomNav from '../pages/Profile/_tests_/BottomNav';
 
 
 export default function RootLayout() {
+const { user } = useAuthContext();
 
   return (
     <>
       <Navbar/>
       <Outlet />
+      {user &&
+        <BottomNav/>
+      }
     </>
   );
 }
--- client/src/pages/Profile/ProfileType.jsx
+++ client/src/pages/Profile/ProfileType.jsx
@@ -2,13 +2,12 @@ import BuyerProfile from './Buyer/BuyerAccount';
 import VendorProfile from './Vendor/VendorAccount';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import Auth from '../../utils/auth';
-import { Typography } from '@mui/material';
 
 export default function Profile() {
 const {user} = useAuthContext()
 
   if (!user) {
-    return <Typography>Oops</Typography>;
+    return null;
   }
   const userType = Auth.getProfile().data.userType;
   return userType === 'buyer' ? <BuyerProfile /> : <VendorProfile />;
