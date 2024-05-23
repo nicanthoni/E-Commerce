17a6eca1faad4753ec5afffa05932a32b1642776
nic-d <nicanthonidiaz@gmail.com>
2024-05-23 16:19:48 -0400

    refactored ProductCard to be cleaner, and minor changes to styling

--- client/src/pages/Explore/Product/ProductCard.jsx
+++ client/src/pages/Explore/Product/ProductCard.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions, Box, Grid } from '@mui/material';
+import { CardActionArea, Box, Grid, Stack } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
@@ -13,15 +13,14 @@ import { Tooltip } from '@mui/material';
 export default function ProductCard() {
   return (
     <Grid container spacing={5} marginTop={4} marginBottom={3}>
-
       {Data.map((result, index) => (
 
+        // Grid item created for each product
         <Grid item xs={12} sm={6} md={4} key={index} align='center'>
-          <Card sx={{ maxWidth: 275, padding: '10px' }}>
+          <Card sx={{ maxWidth: 275, padding: '8px' }}>
 
             {/* Clickable area of card */}
             <CardActionArea component={Link} to={`/product/${result.id}`}>
-
               <Box sx={{ height: '100px', width: '100px' }}>
                 <CardMedia
                   component='img'
@@ -37,11 +36,10 @@ export default function ProductCard() {
               </Box>
 
 
-              {/* Product Card details*/}
-              <CardContent align='left'>
-
+              {/* Card Content */}
+              <CardContent sx={{ paddingBottom: 1}}>
                 {/* name */}
-                <Typography gutterBottom variant='h5' component='div'>
+                <Typography gutterBottom variant='h6'>
                   {result.name}
                 </Typography>
 
@@ -51,45 +49,40 @@ export default function ProductCard() {
                   variant='body2'
                   color='text.secondary'
                   sx={{
-                    // Apply ellipsis and hidden overflow when description content exceeds two lines length
                     display: '-webkit-box',
                     WebkitLineClamp: 2,
                     WebkitBoxOrient: 'vertical',
-                    textOverflow: 'ellipsis',
+                    textOverflow: 'ellipsis',  // ellipsis + hidden overflow when content exceeds 2 lines length
                     overflow: 'hidden',
                   }}
                 >
                   {result.description}
                 </Typography>
               </CardContent>
-
             </CardActionArea>
 
+             
+             {/* Price & Wishlist button stack */}
+             <Stack direction='row' justifyContent='space-around'>
+                
+                <Typography alignSelf='center' fontWeight={'bold'}>${result.price}</Typography>
+              
+                <Box>
+                  <Tooltip title='Add to wishlist' placement='right'>
+                  <Checkbox
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                  />
+                  </Tooltip>
+                </Box>
 
-            <CardActions>
-              <Grid container justifyContent='space-between'>
-                <Grid item>
-                  <Typography fontWeight={'bold'}>${result.price}</Typography>
-                </Grid>
-                <Grid item>
-                  <Box>
-                    <Tooltip title='Add to wishlist' placement='right'>
-                      <Checkbox
-                        icon={<FavoriteBorder />}
-                        checkedIcon={<Favorite />}
-                      />
-                    </Tooltip>
-                  </Box>
-                </Grid>
-              </Grid>
-            </CardActions>
-
-
+            </Stack>
+                
+  
           </Card>
         </Grid>
 
       ))}
-
     </Grid>
   );
 }
