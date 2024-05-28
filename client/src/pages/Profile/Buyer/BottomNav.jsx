fca1c934a82744acf51345bbca103a279d0d6167
nic-d <nicanthonidiaz@gmail.com>
2024-05-28 12:06:24 -0400

    Added inbox nav item to menu drawer

--- client/src/pages/Profile/Buyer/BottomNav.jsx
+++ client/src/pages/Profile/Buyer/BottomNav.jsx
@@ -3,22 +3,15 @@ import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import MailIcon from '@mui/icons-material/Mail';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Paper from '@mui/material/Paper';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import HomeIcon from '@mui/icons-material/Home';
 import { NavLink } from 'react-router-dom';
-import { useAuthContext } from '../../../hooks/useAuthContext';
+
 
 export default function BottomNav() {
-  const { user } = useAuthContext();
   const [active, setActive] = useState(1);
 
-  // console.log('user: ', user.Userlogin) // buyer
-  // console.log('vendor: ', user.Vendorlogin) // vendor
-
-  const isBuyer = user.Userlogin
-  const isVendor = user.Vendorlogin
 
   return (
     <Box>
@@ -51,15 +44,6 @@ export default function BottomNav() {
           />
 
           {/* Cart / Inbox - render depending on user type */}
-          {isBuyer ? ( 
-          <BottomNavigationAction
-            component={NavLink}
-            to='/checkout'
-            label='Checkout'
-            icon={<ShoppingCartIcon />}
-            showLabel
-          />
-        ) : (
           <BottomNavigationAction
             component={NavLink}
             to='/inbox'
@@ -67,8 +51,6 @@ export default function BottomNav() {
             icon={<MailIcon />}
             showLabel
           />
-        )}
-
         </BottomNavigation>
       </Paper>
     </Box>
