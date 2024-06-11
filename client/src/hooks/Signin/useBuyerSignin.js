7b32c40b7c5bf299cf33c25b41fc9b03d61a9baa
nic-d <nicanthonidiaz@gmail.com>
2024-06-11 11:51:00 -0400

    IMplemented corrected signin logic for alerts to displayed to the vendor side as well

--- client/src/hooks/Signin/useBuyerSignin.js
+++ client/src/hooks/Signin/useBuyerSignin.js
@@ -6,42 +6,42 @@ import { buyer_login } from '../../utils/mutations';
 import { useAuthContext } from '../useAuthContext';
 
 export const useBuyerSignin = () => {
-    const [stateError, setStateError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const {dispatch} = useAuthContext()
-    const navigate = useNavigate()
+const [stateError, setStateError] = useState(null)
+const [isLoading, setIsLoading] = useState(null)
+const {dispatch} = useAuthContext()
+const navigate = useNavigate()
 
-    //  Mutation
-    const [LoginUser, { error, loading, data }] = useMutation(buyer_login);
+//  Mutation
+const [LoginUser, { error, loading, data }] = useMutation(buyer_login);
 
-    const signin = async (formState) => {
-        setIsLoading(true)
-        setStateError(null)
+const signin = async (formState) => {
+  setIsLoading(true)
+  setStateError(null)
 
-        try {
-            const { data } = await LoginUser({
-            variables: formState,
-            });
-            
-            // login with users token
-            Auth.login(data.Userlogin.token);
-            
-            // update the auth context
-            dispatch({ type: 'LOGIN', payload: data })
+  try {
+    const { data } = await LoginUser({
+    variables: formState,
+    });
+    
+    // login with users token
+    Auth.login(data.Userlogin.token);
+    
+    // update the auth context
+    dispatch({ type: 'LOGIN', payload: data })
 
-            setIsLoading(false)
+    setIsLoading(false)
 
-            // Indicate a successful login
-            return true;
+    
+    return true; // Indicate a successful login
 
 
-      } catch (e) {
-            setStateError(true)
-            setIsLoading(false)
-            console.error('LoginUser error in useSignin() hook: ', e);
-            return false; // Indicate a failed login
-      }
-         
+    } catch (e) {
+        setStateError(true)
+        setIsLoading(false)
+        console.error('LoginUser error in useSignin() hook: ', e);
+        return false; // Indicate a failed login
     }
-    return { signin, isLoading, stateError }
+        
+}
+return { signin, isLoading, stateError }
 }
