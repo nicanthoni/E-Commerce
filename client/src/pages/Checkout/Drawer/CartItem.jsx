9e6a90ad25ca24130312a17789b2a78f34fc26b7
nic-d <nicanthonidiaz@gmail.com>
2024-06-20 11:56:33 -0400

    removed business logic from delete item button and into parent component Cartitem

--- client/src/pages/Checkout/Drawer/CartItem.jsx
+++ client/src/pages/Checkout/Drawer/CartItem.jsx
@@ -1,11 +1,40 @@
 import { Stack, Box, Typography } from '@mui/material';
+import { useState } from 'react';
 import DeleteItem from '../../../components/Buttons/DeleteItem';
 import QuantityIncrementer from '../../../components/test.QuantityIncrementer';
-import WishlistButton from '../../../components/Buttons/WishlistButton';
+import ItemAlert from '../../../components/Alerts/Items/ItemUpdate';
+import { useCart } from '../../../hooks/Products/useCart';
 
 // For each item in cart
-export default function CartItem({ userData, refetchCart, refetchUserData }) {
-  
+export default function CartItem({
+  userId,
+  userData,
+  refetchCart,
+  refetchUserData,
+}) {
+  // Alert States
+  const [alertMessage, setAlertMessage] = useState('');
+  const [itemAlertVisible, setItemAlertVisible] = useState(false);
+
+  // Hook
+  const { deleteCart, isLoading, stateError } = useCart();
+
+  // onClick of delete button - handle item deletion
+  const handleDeleteItem = async (itemId) => {
+    try {
+      await deleteCart(itemId, userId);
+      setAlertMessage('Removed');
+      setItemAlertVisible(true);
+      refetchCart(); // refetch the updated cart data
+      setTimeout(() => {
+        setItemAlertVisible(false);
+      }, 1000);
+      refetchUserData(); // refetch updated user data
+    } catch (e) {
+      console.log('Delete from cart error:', e);
+    }
+  };
+
   return (
     <>
       {userData.user.cart.map((item, index) => (
@@ -20,9 +49,8 @@ export default function CartItem({ userData, refetchCart, refetchUserData }) {
           justifyContent='flex-start'
           bgcolor='#F2F2F2'
         >
-
           {/* Product img & Incrementer */}
-          <Stack gap={2} >
+          <Stack gap={2}>
             <Box sx={{ height: '100px', width: '100px' }}>
               <img
                 src={item.item.img}
@@ -30,13 +58,12 @@ export default function CartItem({ userData, refetchCart, refetchUserData }) {
                 style={{
                   width: '100%',
                   height: '100%',
-                  objectFit: 'contain', 
+                  objectFit: 'contain',
                 }}
               />
-           </Box>
+            </Box>
             <QuantityIncrementer userData={userData} />
           </Stack>
-
 
           {/* Product details */}
           <Stack direction='column' justifyContent='flex-end'>
@@ -72,12 +99,11 @@ export default function CartItem({ userData, refetchCart, refetchUserData }) {
 
             {/* Buttons  */}
             <DeleteItem
-              itemId={item.item._id}
-              refetchCart={refetchCart}
-              refetchUserData={refetchUserData}
+              onClick={() => handleDeleteItem(item.item._id)} // pass item._id to function as itemId
             />
-
           </Stack>
+          {/* ⚠️ Alerts ⚠️ - visibility controlled by local state */}
+          <ItemAlert visible={itemAlertVisible} message={alertMessage} />
         </Stack>
       ))}
     </>
--- client/src/pages/Checkout/Drawer/CartLayout.jsx
+++ client/src/pages/Checkout/Drawer/CartLayout.jsx
@@ -5,29 +5,37 @@ import Grid from '@mui/material/Grid';
 import { useAuthContext } from '../../../hooks/useAuthContext';
 import { useEffect } from 'react';
 
-
 // Layout of components within the cart drawer
-export default function CartLayout({  loadUser, userData, cartData, refetchCart, refetchUserData  }) {
-const { id } = useAuthContext()
+export default function CartLayout({
+  loadUser,
+  userData,
+  cartData,
+  refetchCart,
+  refetchUserData,
+  userId,
+}) {
+  const { id } = useAuthContext();
 
-// Run loadUser when component mounts
-useEffect(() => {
-  loadUser();
-}, [loadUser]);
+  // Run loadUser when component mounts
+  useEffect(() => {
+    loadUser();
+  }, [loadUser]);
 
+  // Loading check
+  if (!userData || !cartData) {
+    return <Typography>Loading...</Typography>;
+  }
 
- // Loading check
- if (!userData || !cartData) {
-  return <Typography>Loading...</Typography>;
-}
+  // Calculate cart subtotal
+  const subtotal = userData.user.cart
+    .reduce((total, item) => {
+      return total + item.item.price;
+    }, 0)
+    .toFixed(2);
 
-// Calculate cart subtotal 
-const subtotal = userData.user.cart.reduce((total, item) => {
-  return total + item.item.price;
-}, 0).toFixed(2);
-  
   return (
-    <Grid container
+    <Grid
+      container
       direction='column'
       flexWrap='nowrap'
       sx={{
@@ -38,7 +46,8 @@ const subtotal = userData.user.cart.reduce((total, item) => {
       }}
     >
       {/* Cart icon */}
-      <Grid item
+      <Grid
+        item
         sx={{
           bgcolor: 'primary.main',
           flex: '0 0 10%', // Fixed height for the icon section
@@ -56,67 +65,73 @@ const subtotal = userData.user.cart.reduce((total, item) => {
         </Typography>
       </Grid>
 
-
       {/* Items in Cart*/}
-      {cartData.usersCart.length === 0 ?  (
-      <Grid item>
-        <Typography variant='h6' sx={{ m: 2 }}>
-          0 items in your cart.
-        </Typography>
-        <Button
-          href='/explore'
-          variant='contained'
-          color='secondary'
-          sx={{ textTransform: 'none', mx: 3 }}
-          >
-          Shop Items
-        </Button>
-      </Grid>
-      ) : (
-      <Grid item
-        p={1}
-        sx={{
-          overflowY: 'auto',
-          justifyContent: 'flex-start',
-          bgcolor: 'background.paper',
-        }}>
-        <CartItem refetchUserData={refetchUserData} refetchCart={refetchCart} userData={userData} loadUser={loadUser} cartData={cartData}/>
-      </Grid>
-      )} 
-
-
-      {/* Checkout Button & Subtotal */}
-      {cartData.usersCart.length === 0 ? (null) : (
-      <Grid item
-        sx={{
-          flex: '0 0 auto', // Fixed height for the footer section
-          mt: 'auto',
-          bgcolor: 'primary.main',
-        }}
-      >
-        <Box>
-          <Typography padding={1}  color='#fff'>
-            <span style={{ fontWeight: 'bold'}}>Subtotal:</span> ${subtotal}
+      {cartData.usersCart.length === 0 ? (
+        <Grid item>
+          <Typography variant='h6' sx={{ m: 2 }}>
+            0 items in your cart.
           </Typography>
-
           <Button
+            href='/explore'
             variant='contained'
             color='secondary'
-            href='/checkout'
-            sx={{
-              color: 'primary.main',
-              marginBottom: 2,
-              fontWeight: 'bold',
-              textTransform: 'none',
-            }}
+            sx={{ textTransform: 'none', mx: 3 }}
           >
-            Checkout
+            Shop Items
           </Button>
-        </Box>
-      </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          item
+          p={1}
+          sx={{
+            overflowY: 'auto',
+            justifyContent: 'flex-start',
+            bgcolor: 'background.paper',
+          }}
+        >
+          <CartItem
+            userId={userId}
+            refetchUserData={refetchUserData}
+            refetchCart={refetchCart}
+            userData={userData}
+            loadUser={loadUser}
+            cartData={cartData}
+          />
+        </Grid>
       )}
 
+      {/* Checkout Button & Subtotal */}
+      {cartData.usersCart.length === 0 ? null : (
+        <Grid
+          item
+          sx={{
+            flex: '0 0 auto', // Fixed height for the footer section
+            mt: 'auto',
+            bgcolor: 'primary.main',
+          }}
+        >
+          <Box>
+            <Typography padding={1} color='#fff'>
+              <span style={{ fontWeight: 'bold' }}>Subtotal:</span> ${subtotal}
+            </Typography>
 
+            <Button
+              variant='contained'
+              color='secondary'
+              href='/checkout'
+              sx={{
+                color: 'primary.main',
+                marginBottom: 2,
+                fontWeight: 'bold',
+                textTransform: 'none',
+              }}
+            >
+              Checkout
+            </Button>
+          </Box>
+        </Grid>
+      )}
     </Grid>
   );
 }
