141cb40f1340009d70237a88e34231e5bda4da58
nic-d <nicanthonidiaz@gmail.com>
2024-06-26 16:09:30 -0400

    adjustments to inventory data grib column sizing/naming

--- client/src/pages/Profile/Vendor/Inventory.jsx
+++ client/src/pages/Profile/Vendor/Inventory.jsx
@@ -1,13 +1,20 @@
-import { Container, Typography, Box, CircularProgress, IconButton } from '@mui/material';
+import { Container, Typography, Box, CircularProgress } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { useQuery } from '@apollo/client';
 import { Vendor } from '../../../graphql/queries';
-import { useAuthContext } from '../../../hooks/useAuthContext';;
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { delete_Item } from '../../../graphql/mutations';
+import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
 
 export default function Inventory() {
   // Auth context
   const { user, id, type } = useAuthContext();
+
+  // States
+  const [showDeleteIcon, setShowDeleteIcon] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
 
   // Query - Vendor data
   const { loading, error, data } = useQuery(Vendor, {
@@ -20,15 +27,15 @@ export default function Inventory() {
   // Show error if there's an error fetching data
   if (error) return <Typography>Error! {error.message}</Typography>;
 
-  console.log('Vendor Inventory: ', data.vendor.inventory);
+  // console.log('Vendor Inventory: ', data.vendor.inventory);
 
   // Map the fetched data to DataGrid rows
   const rows = data.vendor.inventory.map((item, index) => ({
     id: item._id,
     firstName: item.name,
     lastName: item.category,
-    age: item.inventory,
-    price: item.price,
+    units: item.inventory,
+    price: `$${item.price}`,
     inCart: item.inCart,
     dateCreated: new Date(parseInt(item.createdAt)), // Convert createdAt to Date object
     description: item.description,
@@ -38,42 +45,51 @@ export default function Inventory() {
         : 'N/A',
   }));
 
+  // Handler for when selection model changes
+  const handleSelectionModelChange = (newSelection) => {
+    setSelectedItems(newSelection.selectionModel);
+    console.log('Selected items:', newSelection.selectionModel);
+    setShowDeleteIcon(true);
+  };
+
   return (
     <Container maxWidth='xl'>
       <Box marginTop={14}>
         <DataGrid
           rows={rows}
           columns={[
-            { field: 'id', headerName: 'ID', width: 70 },
+            { field: 'id', headerName: 'ID', width: 100 },
             { field: 'firstName', headerName: 'Item', width: 130 },
             { field: 'lastName', headerName: 'Category', width: 130 },
-            {
-              field: 'age',
-              headerName: 'Inventory',
-              type: 'number',
-              width: 90,
-            },
             {
               field: 'price',
               headerName: 'Price',
               type: 'number',
-              width: 90,
+              width: 80,
             },
+            {
+              field: 'units',
+              headerName: 'Units',
+              type: 'number',
+              width: 65,
+            },
+            {
+              field: 'inCart',
+              headerName: 'InCart',
+              type: 'number',
+              width: 65,
+            },
+
             {
               field: 'rating',
               headerName: 'Rating',
               type: 'number',
-              width: 90,
+              width: 65,
             },
-            {
-              field: 'inCart',
-              headerName: 'In Carts',
-              type: 'number',
-              width: 90,
-            },
+
             {
               field: 'dateCreated',
-              headerName: 'Date created',
+              headerName: 'Created',
               type: 'date',
               width: 90,
             },
@@ -93,6 +109,8 @@ export default function Inventory() {
           }}
           pageSizeOptions={[5, 10, 15, 20]}
           checkboxSelection
+          onSelectionModelChange={handleSelectionModelChange}
+          selectionModel={selectedItems}
         />
       </Box>
     </Container>
